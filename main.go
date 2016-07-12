d7bb5a6fe7e65f9d8567666a93b4c051377d21f9
Óscar López <olopez@tappsi.co>
2016-07-12 15:03:31 -0500

    Revert change: remove dependency on 'secure' middleware, we're going to handle ssl with nginx proxying

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"syscall"
 	"os/signal"
 	"github.com/kataras/iris"
-	"github.com/iris-contrib/middleware/secure"
 	"github.com/tappsi/airbrake-webhook/webhook"
 )
 
@@ -16,24 +15,11 @@ func main() {
 	cfg    := webhook.LoadConfiguration("./config/")
 	queue  := webhook.NewMessagingQueue(cfg.QueueURI, cfg.ExchangeName, cfg.PoolConfig)
 	hook   := webhook.NewWebHook(queue)
-	secure := secureConfig(cfg.SecureConfig)
 
-	iris.UseFunc(func(c *iris.Context) {
-		err := secure.Process(c)
-		webhook.FailOnError(err, "Failed to config secure middleware")
-		c.Next()
-	})
 	iris.Post("/" + cfg.EndpointName, hook.Process)
-
 	go cleanup(queue)
 	iris.Listen(fmt.Sprintf(":%d", cfg.WebServerPort))
 
-}
-
-func secureConfig(cfg webhook.SecureConfiguration) *secure.Secure {
-	return secure.New(secure.Options{
-		IsDevelopment: cfg.IsDevelopment,
-	})
 }
 
 func cleanup(queue webhook.MessagingQueue) {
--- webhook/config.go
+++ webhook/config.go
@@ -13,17 +13,12 @@ type Configuration struct {
 	ExchangeName  string              `json:"exchange-name"`
 	QueueURI      string              `json:"queue-uri"`
 	PoolConfig    PoolConfiguration   `json:"pool-config"`
-	SecureConfig  SecureConfiguration
 }
 
 type PoolConfiguration struct {
 	MaxTotal int `json:"max-total"`
 	MinIdle  int `json:"min-idle"`
 	MaxIdle  int `json:"max-idle"`
-}
-
-type SecureConfiguration struct {
-	IsDevelopment bool `json:"is-development"`
 }
 
 func LoadConfiguration(path string) Configuration {
